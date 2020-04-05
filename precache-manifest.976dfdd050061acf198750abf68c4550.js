1bcf5ae6ceba159a499f920326951b7471e175d9
Amartya Roy <amartyakumar@bitpod.io>
2020-04-05 18:41:02 +0530

    Updates

---
+++ precache-manifest.976dfdd050061acf198750abf68c4550.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e4bde9e2a0f1e35345d147c7740acf1",
+    "url": "/amartyaroy.github.io/index.html"
+  },
+  {
+    "revision": "0fe3ce620a400a467059",
+    "url": "/amartyaroy.github.io/static/css/main.895bfd77.chunk.css"
+  },
+  {
+    "revision": "d6da0e9117acddb5b585",
+    "url": "/amartyaroy.github.io/static/js/2.bf98555e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/amartyaroy.github.io/static/js/2.bf98555e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fe3ce620a400a467059",
+    "url": "/amartyaroy.github.io/static/js/main.9377239d.chunk.js"
+  },
+  {
+    "revision": "1986c8103f50d0ca4602",
+    "url": "/amartyaroy.github.io/static/js/runtime-main.5b231ed0.js"
+  },
+  {
+    "revision": "8a02b12ca27034edf36470520c768b54",
+    "url": "/amartyaroy.github.io/static/media/mypic.8a02b12c.png"
+  }
+]);
